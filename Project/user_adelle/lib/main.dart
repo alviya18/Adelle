a0f2a9f16444ce6f01b9e951b7626e03a5b58bf2
alviya18 <alviyashbu18@gmail.com>
2025-03-17 22:58:43 +0530

    main.dart

--- Project/user_adelle/lib/main.dart
+++ Project/user_adelle/lib/main.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
-
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:user_adelle/screens/home.dart';
 import 'package:user_adelle/screens/login.dart';
 import 'package:user_adelle/screens/welcome.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   await Supabase.initialize(
     url: 'https://afndskoljshrlbvemsyu.supabase.co',
     anonKey:
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFmbmRza29sanNocmxidmVtc3l1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzcyNjk5NDEsImV4cCI6MjA1Mjg0NTk0MX0.OfQrC9UvHGbJrEq2KZwwwQDM8eBhd1h6JjslnUy7iJA',
   );
-  runApp(MainApp());
+
+  runApp(const MainApp());
 }
 
 final supabase = Supabase.instance.client;
@@ -21,28 +24,53 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        // Dark Theme
-
-        home: AuthWrapper());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const AuthWrapper(),
+    );
   }
 }
 
-class AuthWrapper extends StatelessWidget {
+class AuthWrapper extends StatefulWidget {
   const AuthWrapper({super.key});
 
   @override
+  _AuthWrapperState createState() => _AuthWrapperState();
+}
+
+class _AuthWrapperState extends State<AuthWrapper> {
+  bool isFirstTime = true; // Default to true
+
+  @override
+  void initState() {
+    super.initState();
+    checkFirstTimeUser();
+  }
+
+  Future<void> checkFirstTimeUser() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool firstTime = prefs.getBool('isFirstTime') ?? true;
+
+    setState(() {
+      isFirstTime = firstTime;
+    });
+
+    if (isFirstTime) {
+      // Mark that the user has seen WelcomePage
+      await prefs.setBool('isFirstTime', false);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // Check if the user is already logged in
     final session = supabase.auth.currentSession;
 
-    if (session != null) {
-      // User is logged in, navigate to HomePage
-      return HomePage();
+    if (isFirstTime) {
+      return const WelcomePage(); // Show WelcomePage only on first launch
+    } else if (session != null) {
+      return const HomePage(); // If logged in, go to HomePage
     } else {
-      // User is not logged in, navigate to LandingPage
-      return LoginPage();
+      return const LoginPage(); // If not logged in, go to LoginPage
     }
   }
 }
